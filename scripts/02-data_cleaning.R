e056786a7247060a2b8ab9271f9a95baf3c0834b
Junbo Li <junb.li@mail.utoronto.ca>
2024-09-26 17:03:55 -0400

    changed heading of scripts

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -6,7 +6,7 @@
 # Date: 13 SEP 2024
 # Contact: junb.li@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: None
+# Pre-requisites: raw data downloaded
 # Any other information needed? None
 
 #### Workspace setup ####
@@ -47,3 +47,28 @@ quarterly_data <- quarterly_data |>
   filter(Quarter != '2024Q3')
 #### Save data ####
 write_csv(quarterly_data, "data/analysis_data/analysis_data.csv")
+
+# Test 1: Ensure all entries are greater than 0
+test_greater_than_zero <- all(quarterly_data$Total_Population > 0) &
+  all(quarterly_data$Pre_Working >= 0) &
+  all(quarterly_data$Working >= 0) &
+  all(quarterly_data$Retirement >= 0) &
+  all(quarterly_data$Chronic >= 0) &
+  all(quarterly_data$Refugee >= 0)
+
+if(test_greater_than_zero) {
+  print("Test 1 Passed: All entries are greater than or equal to 0.")
+} else {
+  print("Test 1 Failed: Some entries are less than 0.")
+}
+
+# Test 2: Ensure the sum of Pre-working, Working, and Retirement equals the Total Population
+test_sum_equals_total <- all(quarterly_data$Pre_Retirement_and_Retirement_Age + 
+                               quarterly_data$Working_Age + 
+                               quarterly_data$Early_Career_Stage == quarterly_data$actively_homeless)
+
+if(test_sum_equals_total) {
+  print("Test 2 Passed: The sum of Pre-working, Working, and Retirement equals the Total Population.")
+} else {
+  print("Test 2 Failed: The sum of the working categories does not equal the total population.")
+}
